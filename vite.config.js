e2ff93c1a0fcf600595d2527bcb360965f37caf7
Hamzah712 <hamzah.712680678@gmail.com>
2025-08-05 16:53:35 +0500

    Git configuration fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This works for any repository name
+  base: '/Hamzah-Porfolio/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
